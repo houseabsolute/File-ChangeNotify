e53b33b22a1be908146e59e779f1e3c06e6ca332
Dave Rolsky <autarch@urth.org>
2009-05-05 13:47:36 -0500

    adding initial checkin

---
+++ lib/File/ChangeNotify.pm
@@ -0,0 +1,53 @@
+package File::ChangeNotify;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+File::ChangeNotify - The fantastic new File::ChangeNotify!
+
+=head1 SYNOPSIS
+
+XXX - change this!
+
+    use File::ChangeNotify;
+
+    my $foo = File::ChangeNotify->new();
+
+    ...
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+This class provides the following methods
+
+=head1 AUTHOR
+
+Dave Rolsky, C<< <autarch@urth.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-file-changenotify@rt.cpan.org>,
+or through the web interface at L<http://rt.cpan.org>.  I will be
+notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Dave Rolsky, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
